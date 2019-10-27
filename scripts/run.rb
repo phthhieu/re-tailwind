f57d3973634f1fd54fe694088667ecff23985439
Hieu Pham <phthhieu@gmail.com>
2019-10-27 09:39:39 +0700

    Handle single line variant by simply adding alias to string

--- scripts/run.rb
+++ scripts/run.rb
@@ -1,12 +1,18 @@
-def get_utility_code(filename, utility_name, chunk_name)
+def get_classname(str)
   regex = /\.[\w'-]*/
-  options = File.readlines(filename).map do |line|
-    match = line.match(regex)
-    if match
-      classname = match[0].delete('.')
-      variant = classname.split('-').map(&:capitalize).join('')
-      [variant, classname]
-    end
+  match = str.match(regex)
+  if match
+    match[0].delete('.')
+  else
+    ""
+  end
+end
+
+def get_multi_variants_code(lines, utility_name, chunk_name)
+  options = lines.map do |line|
+    classname = get_classname(line)
+    variant = classname.split('-').map(&:capitalize).join('')
+    [variant, classname]
   end.compact
   """
 /* #{utility_name} */
@@ -20,35 +26,69 @@ let #{chunk_name}ToJs =
   """
 end
 
+def get_string_alias_code(line, utility_name, chunk_name)
+  classname = get_classname(line)
+  """
+/* #{utility_name} */
+
+type #{chunk_name} = string;
+
+let #{chunk_name}ToJs = () => \"#{classname}\";
+  """
+end
+
+def chunk_to_js(chunk)
+  utility_name = chunk[0]
+  chunk_name = chunk[1]
+  is_multi_variant_codes = chunk[3]
+  if is_multi_variant_codes
+    "  | #{utility_name}(v) => #{chunk_name}ToJs(v)"
+  else
+    "  | #{utility_name} => #{chunk_name}ToJs()"
+  end
+end
+
+def chunk_to_t(chunk)
+  utility_name = chunk[0]
+  chunk_name = chunk[1]
+  is_multi_variant_codes = chunk[3]
+  if is_multi_variant_codes
+    "  | #{utility_name}(#{chunk_name})"
+  else
+    "  | #{utility_name}"
+  end
+end
+
 def run(inputDir, outputFile)
   regex = /\w*\.txt/
   chunks = Dir[inputDir].map do |filename|
     utility_name = filename.match(regex)[0].gsub(".txt", "")
     chunk_name = utility_name.sub(utility_name[0], utility_name[0].downcase)
-    code = get_utility_code(filename, utility_name, chunk_name)
-    [utility_name, chunk_name, code]
+    lines = File.readlines(filename)
+    is_multi_variant_codes = lines.length > 1
+    chunk_code = if is_multi_variant_codes
+                   get_multi_variants_code(lines, utility_name, chunk_name)
+                 else
+                   get_string_alias_code(lines[0], utility_name, chunk_name)
+                 end
+    [utility_name, chunk_name, chunk_code, is_multi_variant_codes]
   end
 
-  code = """
-#{
-chunks.map do |chunk|
-  chunk[2]
-end.join("\n")
-}
+  code = """#{chunks.map { |chunk| chunk[2] }.join("\n")}
 
 /* TW */
 
 type t =
-#{chunks.map{ |chunk| "  | #{chunk[0]}(#{chunk[1]})" }.join("\n")};
+#{chunks.map{ |chunk| chunk_to_t(chunk) }.join("\n")};
 
 let tToJs =
   fun
-#{chunks.map{ |chunk| "  | #{chunk[0]}(v) => #{chunk[1]}ToJs(v)" }.join("\n")};
+#{chunks.map{ |chunk| chunk_to_js(chunk) }.join("\n")};
 
 
 let make =
   List.fold_left((result, style) => result ++ \" \" ++ tToJs(style), \"\");
-  """
+"""
 
   File.open(outputFile, "w") { |file| file.puts code }
 end
